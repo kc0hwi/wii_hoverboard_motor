ed9669e7dd2834f4584ccb4343a5638ea02b6418
kc0hwi <kc0hwi@Hotmail.com>
2022-08-24 00:12:46 -0500

    WIP differential steering
    A lazy attempt at differential steering....not very elegant

--- wii_motor_test/wii_motor_test.ino
+++ wii_motor_test/wii_motor_test.ino
@@ -1,4 +1,3 @@
-
 /*   Control two scooter motors with wii nunchuck
  *   
  *   https://mad-ee.com/controlling-a-hoverboard-motor-with-a-simple-arduino/
@@ -11,77 +10,114 @@
 
 Accessory nunchuck;
 
-//Pin declarations
-const int PWM_PIN = 9;   // Motor speed 0-255  2-3, 5-12, 44-46 
-const int DIR_PIN = 2;   // Motor Direction
-const int BRAKE_PIN = 3; // Hard brake, active low
+// some constants
+int minSpeed = -128;
+int maxSpeed = 128;
+int lowerDeadZone = -5;
+int upperDeadZone = 5;
 
-// Variables
-bool dir = 0;
-bool brake = 0;
+// intialize some variables
+int leftSpeed = 0;
+int rightSpeed = 0;
+bool leftDir = 0;
+bool leftBrake = 0;
+bool rightDir = 0;
+bool rightBrake = 0;
+
+//Pin declarations
+const int L_PWM_PIN = 9;   // Motor speed 0-255  2-3, 5-12, 44-46 
+const int L_DIR_PIN = 2;   // Motor Direction
+const int L_BRAKE_PIN = 3; // Hard brake, active low
+const int R_PWM_PIN = 10;   
+const int R_DIR_PIN = 5;   
+const int R_BRAKE_PIN = 6; 
 
 void setup() {
-  //Serial.begin(115200); // For debugging
+  Serial.begin(115200);
   nunchuck.begin();
   if (nunchuck.type == Unknown) {
     nunchuck.type = NUNCHUCK;
   }
-  // Set motor pins to outputs
-  pinMode(PWM_PIN, OUTPUT);
-  pinMode(DIR_PIN, OUTPUT);
-  pinMode(BRAKE_PIN, OUTPUT);
-  // Initialize
-  digitalWrite(BRAKE_PIN, false);
-  digitalWrite(DIR_PIN, false);
-  analogWrite(PWM_PIN, 0);
+//    // Set motor pins to outputs
+//  pinMode(PWM_PIN, OUTPUT);
+//  pinMode(DIR_PIN, OUTPUT);
+//  pinMode(BRAKE_PIN, OUTPUT);
+//  // Initialize
+//  digitalWrite(BRAKE_PIN, false);
+//  digitalWrite(DIR_PIN, false);
+//  analogWrite(PWM_PIN, 0);
 }
 
 void loop() {
   nunchuck.readData();    // Read inputs and update maps
-
-  // Read y value
+  
+  // Read x y  values
   int xVal = nunchuck.getJoyX(); // 127 median
   int yVal = nunchuck.getJoyY(); // 128 median
-  // map joystick values to half speed
-  int ySpeed = map(yVal, 0, 255, -128, 128);
+  // map joystick values
+  int yMap = map(yVal, 0, 255, -128, 128);
+  int xMap = map(xVal, 0, 255, -128, 128);
 
   if (nunchuck.getButtonZ()) {
-    //hit the brake
-    digitalWrite(BRAKE_PIN, !brake);
+    // Engage the brakes
+    setBrakes();
   }
-  // If speed is positive, leave dir as is
-  else if (ySpeed > 5) {
-    digitalWrite(BRAKE_PIN, brake);
-    analogWrite(PWM_PIN, ySpeed);
-    digitalWrite(DIR_PIN, dir);
-  } 
-  // If speed is negative, change dir
-  else if (ySpeed < -5) {
-    digitalWrite(BRAKE_PIN, brake);
-    analogWrite(PWM_PIN, -ySpeed);
-    digitalWrite(DIR_PIN, !dir);  
-  } 
-  // If speed is in neutral
-  else if (yVal <= 5 || yVal >= -5){
-    digitalWrite(BRAKE_PIN, brake);
-    analogWrite(PWM_PIN, 0);
+  else if (xMap < 0 && yMap > 0) { 
+    leftSpeed = yMap + xMap;
+    rightSpeed = yMap;
   }
+  else if (xMap > 0 && yMap > 0) {
+    leftSpeed = yMap;
+    rightSpeed = yMap - xMap; 
+  }
+  else if (xMap < 0 && yMap < 0) { // reverse and swinging left
+    leftSpeed =  yMap - xMap;
+    rightSpeed = yMap;
+  } 
+  else if (xMap > 0 && yMap < 0) { // reverse and swinging right
+    leftSpeed = yMap; 
+    rightSpeed = yMap + xMap; 
+  } 
+  else if (xMap < 0 && yMap == 0) {
+    leftSpeed = yMap;
+    rightSpeed = -xMap; // spin left in place
+  } 
+  else if (xMap > 0 && yMap == 0) {
+    leftSpeed = xMap; // spin right in place
+    rightSpeed = yMap;
+  } 
   else {
-    digitalWrite(BRAKE_PIN, brake);
-    analogWrite(PWM_PIN, 0);
+    leftSpeed = 0;
+    rightSpeed = 0;
   }
 
-//  Serial.print("x: ");
-//  Serial.print(nunchuck.getJoyX());
-//  Serial.print("y position: ");
-//  Serial.print(nunchuck.getJoyY());
-//  Serial.print(" y speed: ");
-//  Serial.print(ySpeed);
+  Serial.print("x: ");
+  Serial.print(xMap);
+  Serial.print(" y: ");
+  Serial.print(yMap);
+  Serial.print("    ");
+  Serial.print("left speed: ");
+  Serial.print(leftSpeed);
+  Serial.print(" right speed: ");
+  Serial.print(rightSpeed);
 //  Serial.print("Button: "); 
 //  if (nunchuck.getButtonZ()) Serial.print(" Z "); 
 //  if (nunchuck.getButtonC()) Serial.print(" C "); 
 
-  delay(20);
-};
+  Serial.println();
+  delay(50);
+}
 
+void setLeftMotor(int leftSpeed) {
+   Serial.println('left speed debug');
+}
+
+void setRightMotor(int rightSpeed) {
+   Serial.println('right speed debug');
+}
+
+void setBrakes() {
+  digitalWrite(L_BRAKE_PIN, !leftBrake);
+  digitalWrite(R_BRAKE_PIN, !rightBrake);
+}
 
