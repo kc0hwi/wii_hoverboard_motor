9e1a4d90f271b0e49b98729384518b22d307beb8
kc0hwi <kc0hwi@Hotmail.com>
2022-08-24 23:56:24 -0500

    Add motor control functions

--- wii_motor_test/wii_motor_test.ino
+++ wii_motor_test/wii_motor_test.ino
@@ -33,19 +33,22 @@ const int R_DIR_PIN = 5;
 const int R_BRAKE_PIN = 6; 
 
 void setup() {
-  Serial.begin(115200);
+//  Serial.begin(115200);
   nunchuck.begin();
   if (nunchuck.type == Unknown) {
     nunchuck.type = NUNCHUCK;
   }
-//    // Set motor pins to outputs
-//  pinMode(PWM_PIN, OUTPUT);
-//  pinMode(DIR_PIN, OUTPUT);
-//  pinMode(BRAKE_PIN, OUTPUT);
-//  // Initialize
-//  digitalWrite(BRAKE_PIN, false);
-//  digitalWrite(DIR_PIN, false);
-//  analogWrite(PWM_PIN, 0);
+  // Set motor pins to outputs
+  pinMode(L_PWM_PIN, OUTPUT);
+  pinMode(L_DIR_PIN, OUTPUT);
+  pinMode(L_BRAKE_PIN, OUTPUT);
+  pinMode(R_PWM_PIN, OUTPUT);
+  pinMode(R_DIR_PIN, OUTPUT);
+  pinMode(R_BRAKE_PIN, OUTPUT);
+  // Initialize speed to 0
+  analogWrite(L_PWM_PIN, 0);
+  analogWrite(R_PWM_PIN, 0);
+  // Explicitly disable brakes?
 }
 
 void loop() {
@@ -55,8 +58,8 @@ void loop() {
   int xVal = nunchuck.getJoyX(); // 127 median
   int yVal = nunchuck.getJoyY(); // 128 median
   // map joystick values
-  int yMap = map(yVal, 0, 255, -128, 128);
-  int xMap = map(xVal, 0, 255, -128, 128);
+  int yMap = map(yVal, 0, 255, minSpeed, maxSpeed);
+  int xMap = map(xVal, 0, 255, minSpeed, maxSpeed);
 
   if (nunchuck.getButtonZ()) {
     // Engage the brakes
@@ -86,38 +89,54 @@ void loop() {
     leftSpeed = xMap; // spin right in place
     rightSpeed = yMap;
   } 
-  else {
+  else { // Disable motors
     leftSpeed = 0;
     rightSpeed = 0;
   }
 
-  Serial.print("x: ");
-  Serial.print(xMap);
-  Serial.print(" y: ");
-  Serial.print(yMap);
-  Serial.print("    ");
-  Serial.print("left speed: ");
-  Serial.print(leftSpeed);
-  Serial.print(" right speed: ");
-  Serial.print(rightSpeed);
+//  Serial.print("x: ");
+//  Serial.print(xMap);
+//  Serial.print(" y: ");
+//  Serial.print(yMap);
+//  Serial.print("    ");
+//  Serial.print("left speed: ");
+//  Serial.print(leftSpeed);
+//  Serial.print(" right speed: ");
+//  Serial.print(rightSpeed);
 //  Serial.print("Button: "); 
 //  if (nunchuck.getButtonZ()) Serial.print(" Z "); 
 //  if (nunchuck.getButtonC()) Serial.print(" C "); 
 
-  Serial.println();
+//  Serial.println();
   delay(50);
 }
 
+// not sure if I have to explicitly disable brake on move
 void setLeftMotor(int leftSpeed) {
-   Serial.println('left speed debug');
+  //Serial.println('left speed debug');
+  digitalWrite(L_BRAKE_PIN, leftBrake);
+  analogWrite(L_PWM_PIN, abs(leftSpeed));
+  if (leftSpeed < 0) {
+    digitalWrite(L_DIR_PIN, !leftDir);
+  } else {
+    digitalWrite(L_DIR_PIN, leftDir);
+  }
 }
 
 void setRightMotor(int rightSpeed) {
-   Serial.println('right speed debug');
+  //Serial.println('right speed debug');
+  digitalWrite(R_BRAKE_PIN, rightBrake);
+  analogWrite(R_PWM_PIN, abs(rightSpeed));
+  if (rightSpeed < 0) {
+    digitalWrite(R_DIR_PIN, !rightDir);
+  } else {
+    digitalWrite(R_DIR_PIN, rightDir);
+  }
 }
 
 void setBrakes() {
   digitalWrite(L_BRAKE_PIN, !leftBrake);
   digitalWrite(R_BRAKE_PIN, !rightBrake);
 }
+
 
